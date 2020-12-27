ce455385700d286ad2274cedc61f4311ff3e351e
Restyled.io <commits@restyled.io>
2020-12-27 11:39:24 +0000

    Restyled by clang-format

--- folioreader/src/main/java/com/folioreader/model/sqlite/FolioDatabaseHelper.java
+++ folioreader/src/main/java/com/folioreader/model/sqlite/FolioDatabaseHelper.java
@@ -6,85 +6,80 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class FolioDatabaseHelper extends SQLiteOpenHelper {
-    @SuppressWarnings("unused")
-    private static final String TAG = "SQLiteOpenHelper";
+  @SuppressWarnings("unused")
+  private static final String TAG = "SQLiteOpenHelper";
 
-    private static FolioDatabaseHelper mInstance;
-    private static SQLiteDatabase myWritableDb;
+  private static FolioDatabaseHelper mInstance;
+  private static SQLiteDatabase myWritableDb;
 
-    public static final String DATABASE_NAME = "FolioReader.db";
-    private static final int DATABASE_VERSION = 2;
+  public static final String DATABASE_NAME = "FolioReader.db";
+  private static final int DATABASE_VERSION = 2;
 
-    public static final String KEY_ID = "_id";
-    private final Context mContext;
+  public static final String KEY_ID = "_id";
+  private final Context mContext;
 
-    public FolioDatabaseHelper(final Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        mContext = context;
+  public FolioDatabaseHelper(final Context context) {
+    super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    mContext = context;
+  }
+
+  public static FolioDatabaseHelper getInstance(Context context) {
+    if (mInstance == null) {
+      mInstance = new FolioDatabaseHelper(context);
+    }
+    return mInstance;
+  }
+
+  public static void clearInstance() { mInstance = null; }
+
+  public SQLiteDatabase getMyWritableDatabase() {
+    if ((myWritableDb == null) || (!myWritableDb.isOpen())) {
+      myWritableDb = this.getWritableDatabase();
     }
 
-    public static FolioDatabaseHelper getInstance(Context context) {
-        if (mInstance == null) {
-            mInstance = new FolioDatabaseHelper(context);
-        }
-        return mInstance;
+    return myWritableDb;
+  }
+
+  @Override
+  public void close() {
+    super.close();
+    if (myWritableDb != null) {
+      myWritableDb.close();
+      myWritableDb = null;
     }
+  }
 
-    public static void clearInstance() {
-        mInstance = null;
-    }
+  @Override
+  public final void onCreate(final SQLiteDatabase db) {
+    Log.d("create tables",
+          "****" + HighLightTable.SQL_CREATE + "\n" + BookmarkTable.SQL_CREATE);
+    db.execSQL(HighLightTable.SQL_CREATE);
+    db.execSQL(BookmarkTable.SQL_CREATE);
+  }
 
-    public SQLiteDatabase getMyWritableDatabase() {
-        if ((myWritableDb == null) || (!myWritableDb.isOpen())) {
-            myWritableDb = this.getWritableDatabase();
-        }
+  @Override
+  public final void onUpgrade(final SQLiteDatabase db, final int oldVersion,
+                              final int newVersion) {
+    /* PROTECTED REGION ID(DatabaseUpdate) ENABLED START */
 
-        return myWritableDb;
-    }
+    // TODO Implement your database update functionality here and remove the
+    // following method call!
+    // onUpgradeDropTables(db);
+    // onCreate(db);
+    resetAllPreferences(mContext);
 
-    @Override
-    public void close() {
-        super.close();
-        if (myWritableDb != null) {
-            myWritableDb.close();
-            myWritableDb = null;
-        }
-    }
+    /* PROTECTED REGION END */
+  }
 
-    @Override
-    public final void onCreate(final SQLiteDatabase db) {
-        Log.d("create tables", "****" + HighLightTable.SQL_CREATE + "\n" + BookmarkTable.SQL_CREATE);
-        db.execSQL(HighLightTable.SQL_CREATE);
-        db.execSQL(BookmarkTable.SQL_CREATE);
-    }
+  /**
+   * This basic upgrade functionality will destroy all old data on upgrade
+   */
+  private final void onUpgradeDropTables(final SQLiteDatabase db) {}
 
-    @Override
-    public final void onUpgrade(final SQLiteDatabase db, final int oldVersion,
-                                final int newVersion) {
-        /* PROTECTED REGION ID(DatabaseUpdate) ENABLED START */
-
-        // TODO Implement your database update functionality here and remove the
-        // following method call!
-        //onUpgradeDropTables(db);
-        //onCreate(db);
-        resetAllPreferences(mContext);
-
-        /* PROTECTED REGION END */
-    }
-
-    /**
-     * This basic upgrade functionality will destroy all old data on upgrade
-     */
-    private final void onUpgradeDropTables(final SQLiteDatabase db) {
-
-    }
-
-    /**
-     * Resets all shared preferences
-     *
-     * @param context
-     */
-    private final void resetAllPreferences(Context context) {
-
-    }
+  /**
+   * Resets all shared preferences
+   *
+   * @param context
+   */
+  private final void resetAllPreferences(Context context) {}
 }
--- folioreader/src/main/java/com/folioreader/ui/activity/ContentHighlightActivity.java
+++ folioreader/src/main/java/com/folioreader/ui/activity/ContentHighlightActivity.java
@@ -8,11 +8,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentTransaction;
-
 import com.folioreader.Config;
 import com.folioreader.Constants;
 import com.folioreader.FolioReader;
@@ -22,133 +20,178 @@ import com.folioreader.ui.fragment.HighlightFragment;
 import com.folioreader.ui.fragment.TableOfContentFragment;
 import com.folioreader.util.AppUtil;
 import com.folioreader.util.UiUtil;
-
 import org.readium.r2.shared.Publication;
 
 public class ContentHighlightActivity extends AppCompatActivity {
-    private boolean mIsNightMode;
-    private Config mConfig;
-    private Publication publication;
+  private boolean mIsNightMode;
+  private Config mConfig;
+  private Publication publication;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_content_highlight);
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().hide();
-        }
-
-        publication = (Publication) getIntent().getSerializableExtra(Constants.PUBLICATION);
-
-        mConfig = AppUtil.getSavedConfig(this);
-        mIsNightMode = mConfig != null && mConfig.isNightMode();
-        initViews();
+    setContentView(R.layout.activity_content_highlight);
+    if (getSupportActionBar() != null) {
+      getSupportActionBar().hide();
     }
 
-    private void initViews() {
+    publication =
+        (Publication)getIntent().getSerializableExtra(Constants.PUBLICATION);
 
-        UiUtil.setColorIntToDrawable(mConfig.getCurrentThemeColor(), ((ImageView) findViewById(R.id.btn_close)).getDrawable());
-        findViewById(R.id.layout_content_highlights).setBackgroundDrawable(UiUtil.getShapeDrawable(mConfig.getCurrentThemeColor()));
+    mConfig = AppUtil.getSavedConfig(this);
+    mIsNightMode = mConfig != null && mConfig.isNightMode();
+    initViews();
+  }
 
-        if (mIsNightMode) {
-            findViewById(R.id.toolbar).setBackgroundColor(Color.BLACK);
-            findViewById(R.id.btn_contents).setBackgroundDrawable(UiUtil.createStateDrawable(mConfig.getCurrentThemeColor(), ContextCompat.getColor(this, R.color.black)));
-            findViewById(R.id.btn_highlights).setBackgroundDrawable(UiUtil.createStateDrawable(mConfig.getCurrentThemeColor(), ContextCompat.getColor(this, R.color.black)));
-            findViewById(R.id.btn_bookmarks).setBackgroundDrawable(UiUtil.createStateDrawable(mConfig.getCurrentThemeColor(), ContextCompat.getColor(this, R.color.black)));
+  private void initViews() {
 
-            ((TextView) findViewById(R.id.btn_contents)).setTextColor(UiUtil.getColorList(ContextCompat.getColor(this, R.color.black), mConfig.getCurrentThemeColor()));
-            ((TextView) findViewById(R.id.btn_highlights)).setTextColor(UiUtil.getColorList(ContextCompat.getColor(this, R.color.black), mConfig.getCurrentThemeColor()));
-            ((TextView) findViewById(R.id.btn_bookmarks)).setTextColor(UiUtil.getColorList(ContextCompat.getColor(this, R.color.black), mConfig.getCurrentThemeColor()));
+    UiUtil.setColorIntToDrawable(
+        mConfig.getCurrentThemeColor(),
+        ((ImageView)findViewById(R.id.btn_close)).getDrawable());
+    findViewById(R.id.layout_content_highlights)
+        .setBackgroundDrawable(
+            UiUtil.getShapeDrawable(mConfig.getCurrentThemeColor()));
 
-        } else {
-            ((TextView) findViewById(R.id.btn_contents)).setTextColor(UiUtil.getColorList(ContextCompat.getColor(this, R.color.white), mConfig.getCurrentThemeColor()));
-            ((TextView) findViewById(R.id.btn_highlights)).setTextColor(UiUtil.getColorList(ContextCompat.getColor(this, R.color.white), mConfig.getCurrentThemeColor()));
-            ((TextView) findViewById(R.id.btn_bookmarks)).setTextColor(UiUtil.getColorList(ContextCompat.getColor(this, R.color.white), mConfig.getCurrentThemeColor()));
+    if (mIsNightMode) {
+      findViewById(R.id.toolbar).setBackgroundColor(Color.BLACK);
+      findViewById(R.id.btn_contents)
+          .setBackgroundDrawable(UiUtil.createStateDrawable(
+              mConfig.getCurrentThemeColor(),
+              ContextCompat.getColor(this, R.color.black)));
+      findViewById(R.id.btn_highlights)
+          .setBackgroundDrawable(UiUtil.createStateDrawable(
+              mConfig.getCurrentThemeColor(),
+              ContextCompat.getColor(this, R.color.black)));
+      findViewById(R.id.btn_bookmarks)
+          .setBackgroundDrawable(UiUtil.createStateDrawable(
+              mConfig.getCurrentThemeColor(),
+              ContextCompat.getColor(this, R.color.black)));
 
-            findViewById(R.id.btn_contents).setBackgroundDrawable(UiUtil.createStateDrawable(mConfig.getCurrentThemeColor(), ContextCompat.getColor(this, R.color.white)));
-            findViewById(R.id.btn_highlights).setBackgroundDrawable(UiUtil.createStateDrawable(mConfig.getCurrentThemeColor(), ContextCompat.getColor(this, R.color.white)));
-            findViewById(R.id.btn_bookmarks).setBackgroundDrawable(UiUtil.createStateDrawable(mConfig.getCurrentThemeColor(), ContextCompat.getColor(this, R.color.white)));
+      ((TextView)findViewById(R.id.btn_contents))
+          .setTextColor(
+              UiUtil.getColorList(ContextCompat.getColor(this, R.color.black),
+                                  mConfig.getCurrentThemeColor()));
+      ((TextView)findViewById(R.id.btn_highlights))
+          .setTextColor(
+              UiUtil.getColorList(ContextCompat.getColor(this, R.color.black),
+                                  mConfig.getCurrentThemeColor()));
+      ((TextView)findViewById(R.id.btn_bookmarks))
+          .setTextColor(
+              UiUtil.getColorList(ContextCompat.getColor(this, R.color.black),
+                                  mConfig.getCurrentThemeColor()));
 
-        }
+    } else {
+      ((TextView)findViewById(R.id.btn_contents))
+          .setTextColor(
+              UiUtil.getColorList(ContextCompat.getColor(this, R.color.white),
+                                  mConfig.getCurrentThemeColor()));
+      ((TextView)findViewById(R.id.btn_highlights))
+          .setTextColor(
+              UiUtil.getColorList(ContextCompat.getColor(this, R.color.white),
+                                  mConfig.getCurrentThemeColor()));
+      ((TextView)findViewById(R.id.btn_bookmarks))
+          .setTextColor(
+              UiUtil.getColorList(ContextCompat.getColor(this, R.color.white),
+                                  mConfig.getCurrentThemeColor()));
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            int color;
-            if (mIsNightMode) {
-                color = ContextCompat.getColor(this, R.color.black);
-            } else {
-                int[] attrs = {android.R.attr.navigationBarColor};
-                TypedArray typedArray = getTheme().obtainStyledAttributes(attrs);
-                color = typedArray.getColor(0, ContextCompat.getColor(this, R.color.white));
-            }
-            getWindow().setNavigationBarColor(color);
-        }
+      findViewById(R.id.btn_contents)
+          .setBackgroundDrawable(UiUtil.createStateDrawable(
+              mConfig.getCurrentThemeColor(),
+              ContextCompat.getColor(this, R.color.white)));
+      findViewById(R.id.btn_highlights)
+          .setBackgroundDrawable(UiUtil.createStateDrawable(
+              mConfig.getCurrentThemeColor(),
+              ContextCompat.getColor(this, R.color.white)));
+      findViewById(R.id.btn_bookmarks)
+          .setBackgroundDrawable(UiUtil.createStateDrawable(
+              mConfig.getCurrentThemeColor(),
+              ContextCompat.getColor(this, R.color.white)));
+    }
 
-        loadContentFragment();
-        findViewById(R.id.btn_close).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      int color;
+      if (mIsNightMode) {
+        color = ContextCompat.getColor(this, R.color.black);
+      } else {
+        int[] attrs = {android.R.attr.navigationBarColor};
+        TypedArray typedArray = getTheme().obtainStyledAttributes(attrs);
+        color =
+            typedArray.getColor(0, ContextCompat.getColor(this, R.color.white));
+      }
+      getWindow().setNavigationBarColor(color);
+    }
+
+    loadContentFragment();
+    findViewById(R.id.btn_close).setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        finish();
+      }
+    });
+
+    findViewById(R.id.btn_contents)
+        .setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            loadContentFragment();
+          }
         });
 
-        findViewById(R.id.btn_contents).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadContentFragment();
-            }
+    findViewById(R.id.btn_highlights)
+        .setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            loadHighlightsFragment();
+          }
         });
 
-        findViewById(R.id.btn_highlights).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadHighlightsFragment();
-            }
+    findViewById(R.id.btn_bookmarks)
+        .setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            loadBookmarksFragment();
+          }
         });
+  }
 
-        findViewById(R.id.btn_bookmarks).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadBookmarksFragment();
-            }
-        });
+  private void loadContentFragment() {
+    findViewById(R.id.btn_contents).setSelected(true);
+    findViewById(R.id.btn_bookmarks).setSelected(false);
+    findViewById(R.id.btn_highlights).setSelected(false);
+    TableOfContentFragment contentFrameLayout =
+        TableOfContentFragment.newInstance(
+            publication, getIntent().getStringExtra(Constants.CHAPTER_SELECTED),
+            getIntent().getStringExtra(Constants.BOOK_TITLE));
+    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+    ft.replace(R.id.parent, contentFrameLayout);
+    ft.commit();
+  }
 
-    }
+  private void loadHighlightsFragment() {
+    findViewById(R.id.btn_contents).setSelected(false);
+    findViewById(R.id.btn_bookmarks).setSelected(false);
+    findViewById(R.id.btn_highlights).setSelected(true);
+    String bookId = getIntent().getStringExtra(FolioReader.EXTRA_BOOK_ID);
+    String bookTitle = getIntent().getStringExtra(Constants.BOOK_TITLE);
+    HighlightFragment highlightFragment =
+        HighlightFragment.newInstance(bookId, bookTitle);
+    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+    ft.replace(R.id.parent, highlightFragment);
+    ft.commit();
+  }
 
-    private void loadContentFragment() {
-        findViewById(R.id.btn_contents).setSelected(true);
-        findViewById(R.id.btn_bookmarks).setSelected(false);
-        findViewById(R.id.btn_highlights).setSelected(false);
-        TableOfContentFragment contentFrameLayout = TableOfContentFragment.newInstance(publication,
-                getIntent().getStringExtra(Constants.CHAPTER_SELECTED),
-                getIntent().getStringExtra(Constants.BOOK_TITLE));
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.parent, contentFrameLayout);
-        ft.commit();
-    }
-
-    private void loadHighlightsFragment() {
-        findViewById(R.id.btn_contents).setSelected(false);
-        findViewById(R.id.btn_bookmarks).setSelected(false);
-        findViewById(R.id.btn_highlights).setSelected(true);
-        String bookId = getIntent().getStringExtra(FolioReader.EXTRA_BOOK_ID);
-        String bookTitle = getIntent().getStringExtra(Constants.BOOK_TITLE);
-        HighlightFragment highlightFragment = HighlightFragment.newInstance(bookId, bookTitle);
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.parent, highlightFragment);
-        ft.commit();
-    }
-
-    private void loadBookmarksFragment(){
-        Log.i("loadBookmarksFragment", ": loading bookmark fragment");
-        findViewById(R.id.btn_contents).setSelected(false);
-        findViewById(R.id.btn_highlights).setSelected(false);
-        findViewById(R.id.btn_bookmarks).setSelected(true);
-        String bookId = getIntent().getStringExtra(FolioReader.EXTRA_BOOK_ID);
-        String bookTitle = getIntent().getStringExtra(Constants.BOOK_TITLE);
-        BookmarkFragment bookmarkFragment = BookmarkFragment.newInstance(bookId, bookTitle);
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.parent, bookmarkFragment);
-        ft.commit();
-    }
+  private void loadBookmarksFragment() {
+    Log.i("loadBookmarksFragment", ": loading bookmark fragment");
+    findViewById(R.id.btn_contents).setSelected(false);
+    findViewById(R.id.btn_highlights).setSelected(false);
+    findViewById(R.id.btn_bookmarks).setSelected(true);
+    String bookId = getIntent().getStringExtra(FolioReader.EXTRA_BOOK_ID);
+    String bookTitle = getIntent().getStringExtra(Constants.BOOK_TITLE);
+    BookmarkFragment bookmarkFragment =
+        BookmarkFragment.newInstance(bookId, bookTitle);
+    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+    ft.replace(R.id.parent, bookmarkFragment);
+    ft.commit();
+  }
 }
